5e760e6ea534742ccbdb15f05a368cfab6e68887
Gabor Kalmar <gakalmar@gmail.com>
2023-07-20 13:26:48 +0200

    updated indentations

--- Basics_01.js
+++ Basics_01.js
@@ -53,69 +53,69 @@ LOGICAL OPERATORS (By hierarchy)
 
 VARIABLE, CONSTANT
 
-var a = 'This is a variable' //old way(?)
-let b = 'This is a variable';
-const c + 'This is a constant';
+    var a = 'This is a variable' //old way(?)
+    let b = 'This is a variable';
+    const c + 'This is a constant';
 
 FUNCTIONS
 
-function funcName(par1, par2) {
-    //CODE
-}
+    function funcName(par1, par2) {
+        //CODE
+    }
 
-funcName(arg1, arg2)    //calls the function with the parameters swapped to the input argumetns
+    funcName(arg1, arg2)    //calls the function with the parameters swapped to the input argumetns
 
 RETURN VS. CONSOLE.LOG()
 
 ARRAYS
 
-let arr = ['index0', 'index1', 'index2']
-console.log(arr)
+    let arr = ['index0', 'index1', 'index2']
+    console.log(arr)
 
-arr.push('item1')           //add item to end
-arr.pop('item2')            //remove item from end
-arr.shift('item3')          //add item to start
-arr.unshift('item4')        //remove item from start
-arr[1]                      //returns 2nd item in array with index1
+    arr.push('item1')           //add item to end
+    arr.pop('item2')            //remove item from end
+    arr.shift('item3')          //add item to start
+    arr.unshift('item4')        //remove item from start
+    arr[1]                      //returns 2nd item in array with index1
 
-console.log(arr)
+    console.log(arr)
 
 
 OBJECTS
 
-let person = {
-    attribute1: 'value1',
-    attribute2: 'value2',
-    newObject: {
-        newObjAttr1: 'value3',
-        newObjAttr2: 'value4'
-    },
-    myFunction: function(var1, var2) {
-      console.log('First variable is : ' + var1 + '\n' + 'Second variable is: ' + var2)
+    let person = {
+        attribute1: 'value1',
+        attribute2: 'value2',
+        newObject: {
+            newObjAttr1: 'value3',
+            newObjAttr2: 'value4'
+        },
+        myFunction: function(var1, var2) {
+        console.log('First variable is : ' + var1 + '\n' + 'Second variable is: ' + var2)
+        }
     }
-}
 
-console.log(person.attribute1)                  //prints value1
-console.log(person.newObject.newObjAttr1)       //prints value3
-person.myFunction('TEST1', person.attribute2)   //executes object's function
+    console.log(person.attribute1)                  //prints value1
+    console.log(person.newObject.newObjAttr1)       //prints value3
+    person.myFunction('TEST1', person.attribute2)   //executes object's function
 
 
 LOOPS
 
-arrLoop = ['item1', 'item2', 'item3', 'item4', 'item5']       //this is just an array with 5 elements for better understanding
-for(let i = 0; i < arrLoop.length; i++) {                         //let i = 0 means i variable's initial value is 0" // i < 5 means until when should it be repeated // i++ means that 1 should be added to 'i' after each step
-    console.log(arrLoop[i]);                                      //print arr[0] then arr[1] ... until it reaches limit
-}
+    arrLoop = ['item1', 'item2', 'item3', 'item4', 'item5']       //this is just an array with 5 elements for better understanding
+    for(let i = 0; i < arrLoop.length; i++) {                         //let i = 0 means i variable's initial value is 0" // i < 5 means until when should it be repeated // i++ means that 1 should be added to 'i' after each step
+        console.log(arrLoop[i]);                                      //print arr[0] then arr[1] ... until it reaches limit
+    }
 
-    // break to be used in for loops when you want the code to stop immediately
-    // continue to be used to break current iteration, and moves to the next iteration, so the rest of the code doesn't run for the current one
+        // break to be used in for loops when you want the code to stop immediately
+        // continue to be used to break current iteration, and moves to the next iteration, so the rest of the code doesn't run for the current one
 
-let i = 0;                      //the variable is created outside the while loop
-while(i <= 10) {                //while loop only has 1 condition
-    console.log(i);             //code that runs in the loop (it will print numbers from 0 to 10)
-    i++;                        //add 1 to iteration number
-}
+    let i = 0;                      //the variable is created outside the while loop
+    while(i <= 10) {                //while loop only has 1 condition
+        console.log(i);             //code that runs in the loop (it will print numbers from 0 to 10)
+        i++;                        //add 1 to iteration number
+    }
 
-    //do-while loops are used when the first iteration is always wanted to be executed, even if the first condition is not true
+        //do-while loops are used when the first iteration is always wanted to be executed, even if the first condition is not true
 
 */
