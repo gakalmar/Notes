a44196555cf9f6a4113fe389418ca22cab54751d
Gabor Kalmar <gakalmar@gmail.com>
2023-07-22 15:04:48 +0200

    general updates

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log(parseInt(""));
+function highAndLow(numbers){
+    numbersArray = numbers.split(' ');
+    for (let i = 0; i < numbersArray.length; i++){
+        numbersArray[i] = parseInt(numbersArray[i]);
+    }
+    console.log(typeof numbersArray[1]);
+    console.log(Math.max(...numbersArray));
+    console.log(Math.min(...numbersArray));
+    return [Math.max(numbersArray), Math.min(numbers)]
+}
+
+console.log(highAndLow("1 2 -2 9"));
