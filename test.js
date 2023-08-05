c6fd45e134376ad36fb888f1364443b1f273be0c
Gabor Kalmar <gakalmar@gmail.com>
2023-08-05 14:18:42 +0200

    work with test.js file

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-let a
-a = null
-console.log(a); 
+function reverseWords(str) {
+    let strArray = str.split(" ");
+    let reverseArray = [];
+    for (let word of strArray){
+        let reverseWord = "";
+        for (let i = word.length-1; i >= 0; i--){
+            //console.log(word[i]);
+            reverseWord += word[i];
+        };
+        reverseArray.push(reverseWord);
+    }
+    return reverseArray.join(" ");
+};
+
+
+
+console.log(reverseWords("Testing to see what it does"));
